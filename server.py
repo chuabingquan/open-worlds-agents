394410ec86b23e74651eaf69d98731008cfd74a3
jetnew <notesjet@gmail.com>
2021-05-06 04:49:31 +0800

    Upload

---
+++ server.py
@@ -0,0 +1,37 @@
+import os
+import psutil
+import json
+import random
+from typing import List, Dict
+import requests
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+get_ram = lambda: psutil.Process(os.getpid()).memory_info().rss // 1e6
+
+class WorldModel(BaseModel):
+    """Pydantic model for FastAPI usage"""
+    time: int
+    state: List[list]
+    scores: Dict[str, int]
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"name": "random-agent"}
+
+@app.post("/action/")
+def get_action(state: WorldModel):
+    print("Ram used:", get_ram())
+    return {"action": random.randint(1, 4)}
+
+@app.on_event("startup")
+def start_agent_server():
+    agent_api = "https://" + os.environ['URL']
+    game = "https://open-worlds.herokuapp.com"
+    params = (
+        ('agent_api', agent_api),
+    )
+    response = requests.post(f"{game}/connect/", params=params)
+    print(f"Connection {json.loads(response.text)['result']}.")
